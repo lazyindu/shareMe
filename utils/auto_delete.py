5216ad6db57a3c50fbbaec044adc52a07142a94f
indu <70087467+lazyindu@users.noreply.github.com>
2023-08-28 16:42:37 +0530

    Update auto_delete.py

--- utils/auto_delete.py
+++ utils/auto_delete.py
@@ -3,6 +3,6 @@ import asyncio
 
 
 async def delete_file_after_delay(file_path):
-    await asyncio.sleep(3600)  # Wait for 1 hour (3600 seconds)
+    await asyncio.sleep(60)  # Wait for 1 hour (3600 seconds)
     os.remove(file_path)
     print('============== Message Deleted successfully ===========')
